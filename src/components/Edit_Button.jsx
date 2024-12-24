9b5a339b919f741be9c8db52c062f6b9eb1b0586
Blakely Mayhall <blakely.c.mayhall@gmail.com>
2024-12-23 22:58:42 -0600

    remove uneeded state

--- src/components/Edit_Button.jsx
+++ src/components/Edit_Button.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import "../index.css";
 
 function Edit_Button({ onClick }) {
-    const [editButton, setEditButton] = useState(0);
-
     return <button className="editButton" onClick={onClick}></button>;
 }
 
