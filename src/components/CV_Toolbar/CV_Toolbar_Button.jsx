0408117d20328536e388684589ffcd07536a359f
Blakely Mayhall <blakely.c.mayhall@gmail.com>
2025-01-01 20:26:23 -0600

    implement toolbar with stub buttons

---
+++ src/components/CV_Toolbar/CV_Toolbar_Button.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "../../styles/CV_Toolbar/cv_toolbar.css";
+
+function CV_Toolbar_Button({ buttonID = "", buttonText = "", onClick }) {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const testFunc = () => {
+        setIsClicked(true);
+    };
+
+    return (
+        <button id={buttonID} className="toolbarButton" onClick={onClick}>
+            {buttonText}
+        </button>
+    );
+}
+
+export default CV_Toolbar_Button;
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import "./index.css";
 import CV_Header from "./components/CV_Header/CV_Header.jsx";
 import CV_Objective from "./components/CV_Objective/CV_Objective.jsx";
 import CV_Education from "./components/CV_Education/CV_Education.jsx";
+import CV_Toolbar from "./components/CV_Toolbar/CV_Toolbar.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
+        <CV_Toolbar />
         <CV_Header />
         <CV_Objective />
         <CV_Education />
